81834eb3ea2e89fcc5aaef0a19c0817ba7bb8fec
rockyglen <rockyglen86@gmail.com>
2022-03-17 14:53:54 +0530

    Footer Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import react from "react";
-import Jumbotron from "./components/jumbotron";
-import jumboData from "./fixtures/jumbo";
+import React from "react";
+import { FooterContainer } from "./containers/footer";
+import { JumbotronContainer } from "./containers/jumbotron";
 
-function App() {
+export default function App() {
   return (
-    <Jumbotron.Container>
-      
-      {jumboData.map((item) => {
-        return (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt} />
-            </Jumbotron.Pane>
-          </Jumbotron>
-        );
-      })}
-    </Jumbotron.Container>
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 }
-
-export default App;
